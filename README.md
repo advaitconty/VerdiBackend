4cabf63fe0452f85c8b1d7ad5af54833cdecd923
AdvaitThePro <advaitthepro@outlook.com>
2024-12-21 15:20:05 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# LeafLogic Backend
+
+A backend for the app (codenamed LeafLogic) since the MongoDrivers for Swift are so terrible
+
+`POST: /add`
+- add user
+
+`GET: /leaderboard`
+- get the whole leaderboard
+
+`GET: /existing`
+- get every registered user to prevent clashes on the leaderboard
---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, redirect, url_for, abort
+from pymongo import MongoClient
+import certifi
+
+app = Flask(__name__)
+
+client = MongoClient("mongodb+srv://oddysey:advaitconty@leaflogic.44pa0.mongodb.net/?retryWrites=true&w=majority&appName=LeafLogic",
+                     tlsCAFile=certifi.where())
+
+db = client["leaflogic"]
+collection = db["leaflogic"]
+
+@app.route('/')
+def main():
+    return "server is up!"
+
+@app.route('/leaderboard')
+def leaderboard():
+    items = list(collection.find())
+    for index, _ in enumerate(items):
+        del items[index]["_id"]
+
+    return sorted(items, key=lambda x: x["credits"]) # can reverse sort by adding reverse=True at back
+
+@app.route('/add', methods=["POST"])
+def add():
+    data = request.json
+
+    if len(list(data.keys())) == 2:
+        print("verified!")
+    else:
+        return "error!"
+
+    for index in range(len(list(collection.find()))):
+        print(data)
+        if list(collection.find())[index]["username"] == data["username"]:
+            collection.delete_one({"username": data["username"]})
+            print("deleted old one!")
+
+    collection.insert_one(data)
+
+    return "done!"
+
+@app.route('/existing')
+def existing():
+    items = list(collection.find())
+    names = []
+    for _, item in enumerate(items):
+        names.append(item["username"])
+
+    return names
+
+if __name__ == "__main__":
+    app.run(debug=True)
